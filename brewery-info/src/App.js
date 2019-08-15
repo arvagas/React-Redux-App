20966dfc608dd9192f3ce72487bc326e9018da8e
Arvin Agas <arvagas@gmail.com>
2019-08-15 15:56:34 -0400

    connected component to store

--- brewery-info/src/App.js
+++ brewery-info/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import BrewCard from './components/BrewCard'
 
 function App() {
+  const breweries = useSelector(state => state)
+  
   return (
     <div>
-      
+      {breweries.map(brew => (
+        <BrewCard key={brew.id} brewery={brew} />
+      ))}
     </div>
   )
 }
---
+++ brewery-info/src/components/BrewCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BrewCard = ({ brewery }) => {
+
+    return (
+        <div>
+            <h1>{brewery.name}</h1>
+        </div>
+    )
+}
+
+export default BrewCard
